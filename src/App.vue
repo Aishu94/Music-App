051ee314bfad172395332a0d073731a159a19655
Aishwarya Dongare <aishwaryadongare.pune@gmail.com>
2022-06-28 20:12:21 +1000

    Next and Prev Buttons

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,10 @@
       <section class="player">
         <h2 class="song-title">{{current.title}} - <span>{{current.artist}}</span></h2>
         <div class="controls">
-           <button class="prev">Prev</button>
+           <button class="prev" @click="prev">Prev</button>
            <button class="play" v-if="!isPlaying" @click="play">Play</button>
            <button class="pause" v-else @click="pause">Pause</button>
-           <button class="next">Next</button>
+           <button class="next" @click="next">Next</button>
         </div>
       </section>
       <section class="playlist">
@@ -62,6 +62,27 @@ export default {
     pause () {
       this.player.pause();
       this.isPlaying = false;
+    },
+    prev () {
+      this.index--;
+      if (this.index < 0) {
+        this.index = this.songs.length - 1;
+      }
+      
+      this.current = this.songs[this.index];
+
+      this.play(this.current);
+
+    },
+    next () {
+      this.index++;
+      if (this.index > this.songs.index - 1) {
+        this.index = 0;
+      }
+
+      this.current = this.songs[this.index];
+
+      this.play(this.current);
     }
 
   },
